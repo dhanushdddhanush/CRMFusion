db9762ad4a4f363e3497e350f769324587427617
Dhanush <98016619+dhanushdddhanush@users.noreply.github.com>
2025-06-24 15:30:26 +0530

    Update storage_service.py 111

--- storage_service.py
+++ storage_service.py
@@ -22,7 +22,12 @@ def store_refresh_token(user_id, refresh_token, partition_key="zoho", extra_data
     }
     entity.update(extra_data)
     table_client.upsert_entity(entity)
-
+def delete_refresh_token(user_id, partition_key="zoho"):
+    try:
+        table_client.delete_entity(partition_key=partition_key, row_key=user_id)
+        print(f"Refresh token for user {user_id} in {partition_key} deleted.")
+    except Exception as e:
+        print(f"Error deleting refresh token for {user_id} in {partition_key}: {e}")
 def get_refresh_token(user_id, partition_key="zoho"):
     try:
         entity = table_client.get_entity(partition_key=partition_key, row_key=user_id)
